abf1a19821219732728eda054c517394b1c92b31
theo82 <theoaristi53@gmail.com>
2025-07-02 20:50:13 +0300

    Global Context

---
+++ src/context.jsx
@@ -0,0 +1,16 @@
+import { useContext, useReducer, useEffect, createContext } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+ const greeting = 'hello';
+ return (
+  <AppContext.Provider value={{greeting}}>
+   {children}
+  </AppContext.Provider>
+ )
+}
+
+export const useGlobalContext = () => {
+ return useContext(AppContext);
+}
