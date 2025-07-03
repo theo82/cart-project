00f9cb8b6c8028fe1540d953b19ea22ee06f2ca2
theo82 <theoaristi53@gmail.com>
2025-07-03 11:24:51 +0300

    Increase Amount

--- src/reducer.js
+++ src/reducer.js
@@ -11,6 +11,12 @@ const reducer = (state, action) => {
   if (action.type === CLEAR_CART) {
     return { ...state, cart: new Map() }
   }
+  // Remove item from cart
+  if (action.type === REMOVE) {
+    const newCart = new Map(state.cart)
+    newCart.delete(action.payload.id)
+    return { ...state, cart: newCart }
+  }
   throw new Error(`No matching "${action.type}" action type`)
 }
 
